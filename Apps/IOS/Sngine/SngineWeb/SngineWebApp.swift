9a44cdf879a18a7a459abdd8b34b83b06659becd
zehra2 <shaham_jaffery@hotmail.com>
2024-01-24 16:08:52 +0500

    first commit

---
+++ Apps/IOS/Sngine/SngineWeb/SngineWebApp.swift
@@ -0,0 +1,17 @@
+//
+//  SngineWebApp.swift
+//  SngineWeb
+//
+//  Created by Zamblek Tech
+//
+
+import SwiftUI
+
+@main
+struct SngineWebApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
